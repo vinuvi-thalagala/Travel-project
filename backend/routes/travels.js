0ce216937e22eeba28bfb4a2bb80ed3f2d2102e7
vinuvi-thalagala <vinuvithalagala01@outlook.com>
2025-02-26 09:54:06 +0530

    Implemented travles.js

--- backend/routes/travels.js
+++ backend/routes/travels.js
@@ -1,40 +1,73 @@
 import { Router } from "express";
-import Travel from '../models/Travel.js';
-const router =  Router();
+import Travel from "../models/Travel.js";
 
-router.route("/add").post((req, res) => {
-    const title = req.body.title;
-    const description = req.body.description;
+const router = Router();
 
-    const newTravel = new Travel({
+
+router.route('/add').post(async (req, res) => {
+
+    const {title , description} = req.body;
+
+    const newTravel = new Travel ({
+
         title,
         description
     });
 
-    newTravel.save(),then(() => {
-        res.json("Travel added");
+   await   newTravel.save().then(() => {
+       console.log("Travel story  created successfully");
+   }).catch((err) => {
+    console.log(err)
+   })
+});
+
+router.route('/').get(async (req, res) => {
+
+    await Travel.find().then((travels) => {
+        res.json(travels);
     }).catch((err) => {
         console.log(err);
+    })
+});
+
+router.route('/update/:id').put(async (req, res) => {
+
+    let travelId = req.params.id;
+
+    const {title, description} = req.body;
+
+    const updateTravel = ({
+        title,
+        description
     });
+
+    const update =  await Travel.findByIdAndUpadate(travelId, updateTravel).then(() => {
+        res.status(200).send({status : "Travle story updated successfully"}, update);
+    }).catch((err) => {
+        res.status(500).send({status : "error with updating travel story"});
+    })
 });
 
-router.route("/").get((req, res) => {
-    Travel.find()
-        .then(foundTravels => res.json(foundTravels));
+router.route('/delete/:id').delete(async (req, res) => {
+
+    let travelId = req.params.id;
+
+    await Travel.findByIdAndDelete(travelId).then(() => {
+        res.status(200).send({status : "Travel story deleted successfully"})
+    }).catch((err) => {
+        res.status(500).send({status : "Error with deleting travle story "});
+    })
 });
 
-router.route("/update/:id").put((req, res) => {
-    const id = req.params.id;
-    Travel.findById(id);
-    Travel.title = req.body.title;
-    Travel.description = req.body.description;
-    Travel.save();
-}
-);
+router.route('/get/:id').get(async (req, res) => {
 
-router.route("/delete/:id").delete((req, res) => {
-    const id = req.params.id;
-    Travel.findByIdAndDelete(id);
+    let travelId = req.params.id;
+
+    const travel = await Travel.findById(travelId).then(() => {
+        res.json(travel);
+    }).catch((err) => {
+        res.json(err);
+    })
 });
 
 export default router;
